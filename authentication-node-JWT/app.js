bd097b898e241d79df48f2258f9e90dd36b8eb67
Bruno Moreira Dos Santos <lasgos@hotmail.com>
2022-04-13 15:03:24 -0300

    atualização

--- authentication-node-JWT/app.js
+++ authentication-node-JWT/app.js
@@ -1,1 +1,13 @@
 require('dotenv').config()
+const express = require('express')
+const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+const req = require('express/lib/request')
+const res = require('express/lib/response')
+
+const app = express()
+app.get('/', (req, res) => {
+    res.status(200).json({msg: 'Bem vindo a API!'})
+})
+app.listen(3000)
